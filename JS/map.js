4d5b716609303ae9fc2c35c458ed56e86e1a8f0a
LeoneBeta <leoneftd.if@gmail.com>
2023-01-02 17:39:03 -0300

    add: Flask and Scapy

--- JS/map.js
+++ JS/map.js
@@ -1,14 +1,49 @@
 var btn = document.querySelector('#btn-rota');
 var ip = document.querySelector('#input-ip');
 
+var req = new XMLHttpRequest();
+
 btn.onclick = function(){
     
-    //Ip fornecido no formulário
-    //console.log(ip.value);
+    var myApi = 'http://127.0.0.1:5000/route/' + ip.value;
+    
+    //Request utilizando XMLHttpRequest
+    req.open('GET', myApi);
+    req.onloadend = function(){        
+        resp = req.responseText;
+        console.log('Resultado da API-1', resp);
+        resp_obj = JSON.parse(resp);
+        console.log('Resultado da API-2', resp_obj);
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    //Request utilizando fetch
+    /*
+    fetch(myApi, {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    */
 
     
-    
-    
+
+
+
+
+
     //Geração de mapa
     var map = L.map('map').setView([51.505, -0.09], 13);
 
--- python/tr-map.py
+++ python/tr-map.py
@@ -1,46 +1,59 @@
+#coding: utf-8
+
 from scapy.all import *
+
 import requests
 
+import json
+from flask import Flask
+from flask import request
+app = Flask(__name__)
 
-for i in range(1,100):
-    pkt = IP(dst="8.8.8.8",ttl=i) / UDP(dport=40000)
-    reply = sr1(pkt, verbose=0)
+list_Ips = []
+
+urlIpInfo_1 = 'https://api.ipinfodb.com/v3/ip-city?key=f5ad2b83006363b9227f48569c02468389c49dd65363884ef06598945e9d187d&ip='
+urlIpInfo_2 = '&format=json'
+
+def callApi(reply):
+    #Link completo para API IPInfoDB
+    completeLink = urlIpInfo_1 + reply.src + urlIpInfo_2
+
+    req = requests.get(completeLink)
+    resp = json.loads(req.text)
+
+    currentIP = {"IP": resp["ipAddress"], "Coord": {"lat": resp["latitude"], "lon": resp["longitude"]}}
+
+    list_Ips.append(currentIP)
+
+#def myTraceroute(ipDst):
+
+    for i in range(1,100):
+        pkt = IP(dst=ipDst, ttl=i) / UDP(dport=33434)
+        reply = sr1(pkt, verbose=0)
+        
+        #Retorno vazio, erro ou perdido
+        if reply is None:
+            break
+
+        callApi(reply)
+
+        #Resposta com IP de destino
+        if reply.type == 3:
+            break
+
+    #return list_Ips
 
 
+@app.route('/route/<ipDst>', methods=['GET'])
+def getRoute(ipDst):
 
+    #Reset da lista
+    list_Ips.clear
 
+    #return myTraceroute(ipDst)
 
+    print(ipDst)
+    return ipDst
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#locationLink = 'https://api.ipinfodb.com/v3/ip-city?key=f5ad2b83006363b9227f48569c02468389c49dd65363884ef06598945e9d187d&ip='
-
-
-#ip = input("Forneça o IP: ")
-
-#listLocation = []
-#location = []
-
-#for i in range(1,100):
-#    pkt = IP(dst=ip, ttl=i) / UDP(dport=40404)
-#
-#    reply = sr1(pkt, verbose=0)
-#    if reply is None:
-#        break
-#    print("ip do ", i, "º salto: ", reply.src)
-
-    #completeLink = locationLink + reply.src
-    #stringLocation = requests(completeLink)
+if __name__ == "__main__":
+    app.run(debug=True)
